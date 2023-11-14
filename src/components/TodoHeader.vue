ff78a555f3ed30558e6be8e0b47a85b8ff87097c
max <maxime.hadj@laplateforme.io>
2023-11-14 14:30:49 +0100

    changed header title

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -8,7 +8,7 @@ import { RouterLink } from 'vue-router';
             <nav class="container">
                 <div class="branding">
                     <img src="../assets/logo.png" alt="chaotic evil mamen">
-                    <h1>Kanban for evil guy</h1>
+                    <h1>Evil Kanban</h1>
                 </div>
             <ul class="nav-routes">
                 <RouterLink to="/">Home</RouterLink>
