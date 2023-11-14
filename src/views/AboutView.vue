d0322913fb28fbabc1c95b6b92cbc20e0c7d0901
thomas-dellacase <thomas.dellacase@laplateforme.io>
2023-11-14 13:20:22 +0100

    about home nav and emit data

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
+    <h1>On est les mechants</h1>
+
+
+<p>Need to plot the downfall of a rival faction? Or perhaps orchestrate the kidnapping of a noble's heir? Use our Evil Planning Board to organize your malevolent masterpieces.
+Grocery List of Doom:</p><br>
+
+<p>Even chaotic evil characters need to eat! Plan your visits to the local markets with the Grocery List of Doom. Ensure you have all the essential ingredients for your next diabolical feast.
+Minion Management:</p><br>
+
+<p>Keep track of your loyal minions and their assigned tasks. Whip them into shape and make sure they're ready for whatever nefarious deeds you have in store.
+Villainous Calendar:</p><br>
+
+<p>Stay on top of important dates, like when your sworn enemies are celebrating birthdays or when the next celestial alignment occurs for your dark rituals. Never miss an opportunity to sow chaos again.
+Treasure Hoard Tracker:</p><br>
+
+<p>Manage your ill-gotten gains with the Treasure Hoard Tracker. Keep tabs on your stolen riches and artifacts, ensuring your hoard remains impressive and envy-inducing.
+Anonymous Chat for Henchmen:</p><br>
+
+<p>Connect with your henchmen securely and anonymously. Discuss plans, issue orders, or just vent about the hardships of being chaotic. No paladins allowed!</p><br>
+<p>Security:</p><br>
+
+<p>Rest assured, your diabolical data is protected by the strongest enchantments and infernal firewalls. Our developers have consulted with the finest warlocks and wizards to ensure your plans remain your own.
+</p><br>
+
+<p>Sign up now and embrace the chaos!
+"ChaoticTasks" - Because even villains need a plan.</p>
   </div>
 </template>
 
---
+++ src/views/TodoView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import{ uid } from 'uid';
+import TodoCreator from '../components/TodoCreator.vue';
+import { ref } from 'vue';
+const todoList = ref([]);
+const createTodo = (todo) =>{
+  todoList.value.push({
+    id: uid(),
+    todo,
+    isCompleted: null,
+    isEditing: null,
+  });
+}
+</script>
+
+<template>
+  <main>
+    <h1>Créer une liste de taches diabolique</h1>
+    <TodoCreator @create-todo="createTodo" />
+  </main>
+</template>
+
+<style scoped lang="scss">
+main {
+  display: flex;
+  flex-direction: column;
+  max-width: 500px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 40px 16px;
+
+  h1 {
+    margin-bottom: 16px;
+    text-align: center;
+  }
+}
+</style>
