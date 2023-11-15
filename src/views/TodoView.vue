4bd190aaf23d1a2e7d719436b76e52bce25725f1
thomas-dellacase <thomas.dellacase@laplateforme.io>
2023-11-15 13:30:32 +0100

    TodoItem et TodoView

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import{ uid } from 'uid';
-import TodoCreator from '../components/TodoCreator.vue';
+import { Icon } from '@iconify/vue';
 import { ref } from 'vue';
+import TodoCreator from '../components/TodoCreator.vue';
 import TodoItem from '../components/TodoItem.vue';
 const todoList = ref([]);
+
 const createTodo = (todo) =>{
   todoList.value.push({
     id: uid(),
@@ -18,11 +20,16 @@ const createTodo = (todo) =>{
   <main>
     <h1>Créer une liste de taches diabolique</h1>
     <TodoCreator @create-todo="createTodo" />
-    <ul>
-      <TodoItem/>
+    <ul class="todo-list" v-if="todoList.length > 0">
+      <TodoItem v-for="todo in todoList" :todo="todo" />
     </ul>
+    <p class="todo-msg" v-else>
+      <Icon icon="game-icons:evil-book" color="red" />
+      <span>Vous n'evez pas de liste de mefaits !!! Les forces du bien vont reprendre le dessus.</span>
+    </p>
   </main>
 </template>
+
 
 <style scoped lang="scss">
 main {
@@ -36,6 +43,22 @@ main {
   h1 {
     margin-bottom: 16px;
     text-align: center;
+  }
+
+  .todo-list {
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    margin-top: 24px;
+    gap: 20px;
+  }
+
+  .todos-msg {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 8px;
+    margin-top: 24px;
   }
 }
 </style>
