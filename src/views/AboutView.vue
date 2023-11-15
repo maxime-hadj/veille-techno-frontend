d33b96e27f16f60c5d4881f809cf99868f8268cd
max <maxime.hadj@laplateforme.io>
2023-11-15 16:00:40 +0100

    added style for about page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,31 +1,39 @@
+<script setup>
+import { Icon } from '@iconify/vue';
+</script>
+
 <template>
   <div class="about">
-    <h1>On est les mechants</h1>
+    <h1>On est les méchants !</h1>
 
 
-<p>Need to plot the downfall of a rival faction? Or perhaps orchestrate the kidnapping of a noble's heir? Use our Evil Planning Board to organize your malevolent masterpieces.
-Grocery List of Doom:</p><br>
+    <p>Besoin de conspirer la chute d'un royaume ennemi ? Ou peut-être d'orchestrer l'enlèvement d'un noble héritier ? Utilisez "Evil Tasks" pour planifier vos desseins malfaisants.</p><br>
 
-<p>Even chaotic evil characters need to eat! Plan your visits to the local markets with the Grocery List of Doom. Ensure you have all the essential ingredients for your next diabolical feast.
-Minion Management:</p><br>
+    <h3> <Icon icon="game-icons:evil-book" color="red" /> Grocery List of Doom</h3>
 
-<p>Keep track of your loyal minions and their assigned tasks. Whip them into shape and make sure they're ready for whatever nefarious deeds you have in store.
-Villainous Calendar:</p><br>
+    <p>Even chaotic evil characters need to eat! Plan your visits to the local markets with the Grocery List of Doom. Ensure you have all the essential ingredients for your next diabolical feast.</p><br>
 
-<p>Stay on top of important dates, like when your sworn enemies are celebrating birthdays or when the next celestial alignment occurs for your dark rituals. Never miss an opportunity to sow chaos again.
-Treasure Hoard Tracker:</p><br>
+    <h3> <Icon icon="game-icons:evil-minion" color="red" /> Minion Management</h3>
 
-<p>Manage your ill-gotten gains with the Treasure Hoard Tracker. Keep tabs on your stolen riches and artifacts, ensuring your hoard remains impressive and envy-inducing.
-Anonymous Chat for Henchmen:</p><br>
+    <p>Keep track of your loyal minions and their assigned tasks. Whip them into shape and make sure they're ready for whatever nefarious deeds you have in store.</p><br>
 
-<p>Connect with your henchmen securely and anonymously. Discuss plans, issue orders, or just vent about the hardships of being chaotic. No paladins allowed!</p><br>
-<p>Security:</p><br>
+    <h3> <Icon icon="game-icons:evil-moon" color="red" /> Villainous Calendar</h3>
 
-<p>Rest assured, your diabolical data is protected by the strongest enchantments and infernal firewalls. Our developers have consulted with the finest warlocks and wizards to ensure your plans remain your own.
-</p><br>
+    <p>Stay on top of important dates, like when your sworn enemies are celebrating birthdays or when the next celestial alignment occurs for your dark rituals. Never miss an opportunity to sow chaos again.</p><br>
 
-<p>Sign up now and embrace the chaos!
-"ChaoticTasks" - Because even villains need a plan.</p>
+    <h3> <Icon icon="game-icons:evil-wings" color="red" /> Treasure Hoard Tracker</h3>
+
+    <p>Manage your ill-gotten gains with the Treasure Hoard Tracker. Keep tabs on your stolen riches and artifacts, ensuring your hoard remains impressive and envy-inducing.</p><br>
+
+    <h3> <Icon icon="game-icons:evil-eyes" color="red" /> Anonymous Chat for Henchmen</h3>
+
+    <p>Connect with your henchmen securely and anonymously. Discuss plans, issue orders, or just vent about the hardships of being chaotic. No paladins allowed!</p><br>
+
+    <h3> <Icon icon="game-icons:evil-tower" color="red" /> Security</h3>
+
+    <p>Rest assured, your diabolical data is protected by the strongest enchantments and infernal firewalls. Our developers have consulted with the finest warlocks and wizards to ensure your plans remain your own.</p><br>
+
+    <p>Inscrivez-vous et embracez le chaos ! "Evil Tasks" - Parce que les méchants ont besoin d'un plan.</p>
   </div>
 </template>
 
@@ -34,6 +42,7 @@ Anonymous Chat for Henchmen:</p><br>
   .about {
     min-height: 100vh;
     display: flex;
+    flex-direction: column;
     align-items: center;
   }
 }
